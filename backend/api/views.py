b5db94767478f9dd514cff5d91077469767829b1
jc mbisa <jcmbisa@gmail.com>
2023-05-05 13:39:08 +0300

    serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,47 @@
 from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 import json
-
+from rest_framework.response import Response
 from products.models import Product
 
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from rest_framework import status
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data={}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    """
+    DRF API View
+    """
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    else:
+        errors = serializer.errors
+        return Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# @api_view(['GET'])
+# def api_home(request, *args, **kwargs):
+#     instance = Product.objects.all().order_by("?").first()
+#     data={}
+#     if instance:
+#         # data = model_to_dict(instance, fields=['id', 'title', 'price','sale_price'])
+#         data = ProductSerializer(instance).data
+#     return Response(data)
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data={}
+#     if model_data:
+#         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+#     return JsonResponse(data)
     #     print(data)
     #     json_data_str = json.dumps(data)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 
-get_response = requests.get(endpoint, params= {"abc":123},json={"query":"Hello World"}) # HTTP Request
+get_response = requests.post(endpoint, params= {"abc":123},json={"title":"hello python",}) # HTTP Request
 print(get_response.headers)
 print(get_response.text)
 
